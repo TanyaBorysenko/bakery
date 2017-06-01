3ba64c4b32295719bcd03672028d61b76963ac41
Tanya <Tanya19941004@gmail.com>
2017-06-01 18:07:07 +0300

    changes in jQ

--- js/my_script.js
+++ js/my_script.js
@@ -1,13 +1,8 @@
-// 1 - визначити обэкт наа яким треба щось зробити
-// 2 - визнаити подію при якій має щось статися
-// 3 - визначити що саме має статися з обєктом при події
+$(document).ready(function(){
 
+    $('.center-block-second a img').click(function (event) {
 
- $(document).ready(function(){
-
-     /*$('.block_img_first a img').click(function (event) {
-        // function body
-        event.preventDefault(); // запобігання стандартній поведінці елемента (в даному випадку перехід по посиланню
+        event.preventDefault();
 
         if ($(this).attr('src') !== 'img/star-empty.png') {
             $(this).attr('src', 'img/star-empty.png');
@@ -15,38 +10,47 @@
             $(this).attr('src', 'img/star-full.png');
         }
 
-        // end
+    });
 
-        // при наведенні на зірку її колір змінються на протилежний і навпаки коли забираєш курсор
+    $('.center-block-second a img').mouseover(function () {
 
-    });*/
+        if ($(this).attr('src') !== 'img/star-empty.png') {
+            $(this).attr('src', 'img/star-empty.png');
+        } else
+        {
+            $(this).attr('src', 'img/star-full.png');
+        }
+    });
 
-     $('.block_img_first a img').mouseover(function () {
+    $('.center-block-second a img').mouseleave(function () {
+        if ($(this).attr('src') !== 'img/star-empty.png') {
+            $(this).attr('src', 'img/star-empty.png');
+        } else
+        {
+            $(this).attr('src', 'img/star-full.png');
+        }
+    });
 
-         if ($(this).attr('src') !== 'img/star-empty.png') {
+    $('.button_black').mouseover(function () {
+        $(this).css({"border":"2px dashed white", "font-size":"27px"})
+    });
 
-             //$(this).onmouseover(function () {
-                 $(this).attr('src', 'img/star-empty.png');
-             //});
-         } else
-             {
-                 //$(this).mouseleave(function () {
-                     $(this).attr('src', 'img/star-full.png');
-             }
-     })
+    $('.button_black').mouseleave(function () {
+        $(this).css({"border":"1px dashed white", "font-size":"20px"})
+    });
 
-$('.block_img_first a img').mouseleave(function () {
-    if ($(this).attr('src') !== 'img/star-empty.png') {
-        $(this).attr('src', 'img/star-empty.png');
-    } else
-    {
-        $(this).attr('src', 'img/star-full.png');
-    }
+    $('.button_transparent').mouseover(function () {
+        $(this).css({"border":"2px solid black", "font-size":"27px"})
+    });
+
+    $('.button_transparent').mouseleave(function () {
+        $(this).css({"border":"1px solid black", "font-size":"20px"})
+    });
+    $('.button_white').mouseover(function () {
+        $(this).css({"border":"2px dashed #dfc691", "font-size":"27px"})
+    });
+
+    $('.button_white').mouseleave(function () {
+        $(this).css({"border":"1px dashed #dfc691", "font-size":"20px"})
+    });
 });
-     $('.button_black').mouseover(function () {
-         $(this).css({"border":"3px dashed white", "font-size":"27px"})
-     })
-     $('.button_black').mouseleave(function () {
-         $(this).css({"border":"1px dashed white", "font-size":"20px"})
-     })
- });
