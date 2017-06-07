09c9fe74b16835fefa7cca939b99975ce7418381
Tanya <Tanya19941004@gmail.com>
2017-06-07 11:13:42 +0300

    changed buttons for carousel

--- js/my_script.js
+++ js/my_script.js
@@ -12,29 +12,6 @@ $(document).ready(function(){
 
     });
 
-    $('.button_black').mouseover(function () {
-        $(this).css({"border":"2px dashed white", "font-size":"27px"})
-    });
-
-    $('.button_black').mouseleave(function () {
-        $(this).css({"border":"1px dashed white", "font-size":"20px"})
-    });
-
-    $('.button_transparent').mouseover(function () {
-        $(this).css({"border":"2px solid black", "font-size":"27px"})
-    });
-
-    $('.button_transparent').mouseleave(function () {
-        $(this).css({"border":"1px solid black", "font-size":"20px"})
-    });
-    $('.button_white').mouseover(function () {
-        $(this).css({"border":"2px dashed #dfc691", "font-size":"27px"})
-    });
-
-    $('.button_white').mouseleave(function () {
-        $(this).css({"border":"1px dashed #dfc691", "font-size":"20px"})
-    });
-
     $('.autoplay').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
